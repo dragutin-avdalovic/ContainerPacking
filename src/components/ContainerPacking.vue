be430526b34e4635a20a13f4745233a36a7bba61
dragutin-avdalovic <dragutin.avdalovic@walter.ba>
2018-04-11 11:21:26 +0200

    First version of the container packaging using canvas and dummy data created

---
+++ src/components/ContainerPacking.vue
@@ -0,0 +1,131 @@
+<template>
+  <canvas ref="myCanvas" width="700" height="300" style="border:1px solid #aa22ff ;">
+    Your browser does not support the HTML5 canvas tag.
+  </canvas>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    // Function to filter units
+    getUnits: function () {
+    }
+  },
+  mounted () {
+    var c = this.$refs.myCanvas
+    var ctx = c.getContext('2d')
+    var arr = [
+      {
+        X: 0,
+        Y: 0,
+        W: 150,
+        H: 100
+      },
+      {
+        X: 150,
+        Y: 0,
+        W: 60,
+        H: 100
+      },
+      {
+        X: 0,
+        Y: 100,
+        W: 60,
+        H: 100
+      },
+      {
+        X: 150,
+        Y: 100,
+        W: 60,
+        H: 100
+      },
+      {
+        X: 60,
+        Y: 100,
+        W: 60,
+        H: 50
+      },
+      {
+        X: 60,
+        Y: 150,
+        W: 60,
+        H: 50
+      },
+      {
+        X: 120,
+        Y: 100,
+        W: 30,
+        H: 70
+      }
+    ]
+
+    arr.map(el => {
+      Object.defineProperty(el, 'selected', {
+        enumerable: true,
+        configurable: true,
+        writable: true,
+        value: false
+      })
+      return el
+    })
+
+    function createBoxes () {
+      arr.forEach(el => {
+        ctx.strokeStyle = '#000000'
+        if (el.selected) {
+          ctx.strokeStyle = '#FF0000'
+        }
+        ctx.strokeRect(el.X, el.Y, el.W, el.H)
+      })
+      ctx.stroke()
+    }
+
+    function getMousePos (canvas, evt) {
+      var rect = c.getBoundingClientRect()
+      return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+      }
+    }
+
+    c.addEventListener('click', function (evt) {
+      var mousePos = getMousePos(c, evt)
+      console.log(mousePos.x, mousePos.y)
+      arr.map(el => {
+        el.selected = false
+        if ((mousePos.x > el.X && mousePos.x < (el.X + el.W)) && (mousePos.y > el.Y && mousePos.y < (el.Y + el.H))) {
+          el.selected = true
+        }
+        return el
+      })
+      createBoxes()
+    }, false)
+
+    createBoxes()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
